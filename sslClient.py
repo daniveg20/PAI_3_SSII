2ee69795f5280d40d112c9c3746deec14059595d
Ismael <ismaelmesazyout@gmail.com>
2022-10-24 14:45:39 +0200

    Terminado

--- sslClient.py
+++ sslClient.py
@@ -1,13 +1,25 @@
 import socket
 import ssl
-import random
-import pprint
+import getpass
+import time
+
+inicio = time.time()
+
 def gen_transacciones():
-    cuenta_Or   = random.randint(0,99999)
-    cuenta_Dest = 65478
-    cantidad    = random.randint(0,1000000)
-    cadena = str(cuenta_Or) + " " + str(cuenta_Dest) + " " + str(cantidad)
+    for _ in range(300):
+        usuario   = input("Nombre de Usuario: ")
+        correo = input("Correo: ")
+        try:
+            x = getpass.getpass()
+        except Exception as err:
+            print('ERROR:', err)
+        if "@" in correo:
+            cadena = "el nombre de usuario es: " + str(usuario) + " ,su correo: " + str(correo) + " y su contraseña: " + str(x)
+            break
+        else:
+            print("Ha escrito mal el correo")
     return cadena
+
 
 file = open("./conf.txt", "r")
 config = []
@@ -19,7 +31,7 @@ num_pruebas = config[0]
 file.close()
 
 hostname = '127.0.0.1'
-# PROTOCOL_TLS_CLIENT requires valid cert chain and hostname
+
 context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 context.load_verify_locations('./certchain.pem')
 context.verify_mode = ssl.CERT_REQUIRED
@@ -27,15 +39,24 @@ context.check_hostname = True
 
 cipher = 'DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA:ECDHE-ECDSA-AES128-GCM-SHA256'
 context.set_ciphers(cipher)
-for i in range(int(num_pruebas)):
+#for i in range(int(num_pruebas)):
+cont = 0
+for _ in range(int(num_pruebas)):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.settimeout(10)
-            wrappedSocket = ssl.wrap_socket(sock, ssl_version=ssl.PROTOCOL_TLSv1_2)
-            msgInput = gen_transacciones()
-            print(msgInput)    
-            wrappedSocket.connect((hostname, 8443))
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(10)
+        wrappedSocket = ssl.wrap_socket(sock, ssl_version=ssl.PROTOCOL_TLSv1_2)
+        wrappedSocket.connect((hostname, 8443))
+        if(cont==0):
+            msgInput = gen_transacciones()   
             wrappedSocket.send(msgInput.encode())
+            time.sleep(0.5)
+            cont +=1
+        msgInput1 = "Se establecio la conexion"
+        wrappedSocket.send(msgInput1.encode())
+        time.sleep(0.5)
+print("\n")
+final = time.time()
 
-
-
+tiempoFinal = final - inicio
+print(tiempoFinal)
--- sslServer.py
+++ sslServer.py
@@ -13,11 +13,10 @@ for line in file:
     config.append(words[1])
 num_pruebas = config[0]
 file.close()
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
-    sock.bind(('127.0.0.1', 8443))
-    for i in range(int(num_pruebas)):
-        sock.listen(5)
+while(True):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
+        sock.bind(('127.0.0.1', 8443))
+        sock.listen(2100)
         with context.wrap_socket(sock, server_side=True) as ssock:
             conn, addr = ssock.accept()
             with conn:
